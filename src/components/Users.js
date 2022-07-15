c44712d4251f9c791d7a448dedc2f3d3b305ed7e
Ryan Cabanas <ryangithub777@gmail.com>
2022-07-14 22:00:43 -0400

    Lesson 167: Any variables that contain any JSX code ***must*** be placed inside of the `render` function for class-based components.
    - I was wrongly under the impression that you could put part of your JSX code
      ***outside*** of the `render` function for class-based components, but this is
      not true.  I was running into errors.
    
      Any variables containing any JSX code must ***all*** live within the `render`
      function.

--- src/components/Users.js
+++ src/components/Users.js
@@ -25,21 +25,21 @@ class Users extends Component {
     });
   }
 
-  usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
   render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
     return (
       <div className={classes.users}>
         <button onClick={this.toggleUsersHandler.bind(this)}>
           {this.state.showUsers ? 'Hide' : 'Show'} Users
         </button>
-        {this.state.showUsers && this.usersList}
+        {this.state.showUsers && usersList}
       </div>
     );
   }
